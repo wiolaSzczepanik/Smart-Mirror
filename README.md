892702cdfcd3fcf9469194e2b1e8de3ec2d3842b
wiolaSzczepanik <wiolaszcz@gmail.com>
2020-02-24 22:36:53 +0100

    Make webside more responsive, add section with date and time

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Smart-Mirror
+
+To nie opis techniczny, raczej notatki co ma się znaleźć na stronie 
+    * zegar
+    * rozkład jazdy z najbliższymi odjazdami tramwajów/autibusów
+    * pogoda (czy bedzie padać, czy nie, potrzebny parsol czy nie)
+    * info ile kredytu już spłaciliśmy
+    * to z najważniejszymy sprawami do załatwienia w danym tygodniu
+    * kalendarz ze zbliżającymi się wydarzeniami
+    
---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let date = startTime();
+document.getElementById("date").innerHTML = date;
+
+
+function startTime() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date').innerHTML = today.toDateString() + " " + h + ":" + m + ":" + s;
+    let t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
